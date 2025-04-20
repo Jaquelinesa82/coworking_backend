ac923bb9c95d884898bba942188350934e854131
jaqueline <jaquelinesa.82@gmail.com>
2025-04-20 11:54:59 -0300

    add factories.py data faker

---
+++ auth_app/factories.py
@@ -0,0 +1,15 @@
+import factory
+from faker import Faker
+from auth_app.models import CustomUser
+import uuid
+fake = Faker()
+
+
+class CustomUserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = CustomUser
+
+    email = factory.LazyAttribute(lambda _: f"{fake.unique.email()}_{uuid.uuid4()}")
+    password = factory.PostGenerationMethodCall('set_password', '101010')
+    first_name = factory.LazyAttribute(lambda _: fake.first_name())
+    last_name = factory.LazyAttribute(lambda _: fake.last_name())
